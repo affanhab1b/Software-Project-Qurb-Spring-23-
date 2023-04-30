4b0cb18cebb54f48cd462deedf943ecb32937952
Daniyal Murtaza <86107799+Daniyal-Murtaza@users.noreply.github.com>
2023-04-30 22:22:38 +0500

    updated-reset-19

--- lib/screens/reset.dart
+++ lib/screens/reset.dart
@@ -16,7 +16,7 @@ class ResetScreen extends StatefulWidget {
   _ResetScreenState createState() => _ResetScreenState();
 }
 
-// Extending
+// Extending reset screen
 class _ResetScreenState extends State<ResetScreen> {
   late String email;
   final auth = FirebaseAuth.instance;
